3795afe307db798ce49c2192b366046ad2149cb2
Ira <iraf333@gmail.com>
2022-06-25 23:02:02 -0400

    add in forked front end, git was not tracking due to forking inside of a git folder

---
+++ hello-world-part-four-tutorial-here/completed/helloworld2.js
@@ -0,0 +1,271 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  helloWorldContract,
+  connectWallet,
+  submitTruth,
+  viewTruth,
+  getCurrentWalletConnected,
+  viewTitle,
+  mintVoters,
+  voteTruth,
+  returnvotes,
+  tallyVotesVerifyTruth,
+} from "./util/interact.js";
+
+import alchemylogo from "./alchemylogo.svg";
+
+const HelloWorld = () => {
+  //state variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [truth, setViewTruth] = useState(""); //default message
+  const [id, setId] = useState("");
+  const [newTruthTitle, setNewTruthTitle] = useState("");
+  const [newTruthURL, setNewTruthUrl] = useState("");
+  const [voter, setNewVoter] = useState("");
+  const [torf, setTOrF] = useState("");
+
+  //called only once
+  useEffect(async () => {
+    addSmartContractListener();
+
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  function addSmartContractListener() {
+    helloWorldContract.events.Title({}, (error, data) => {
+      if (error) {
+        setStatus("😥 " + error.message);
+      } else {
+        setViewTruth(data.returnValues[1]);
+        setStatus("🎉 Your message has been updated!");
+      }
+    });
+  }
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onUpdatePressed = async () => {
+    const { status } = await submitTruth(walletAddress, newTruthTitle, newTruthURL);
+    setStatus(status);
+  };
+
+  const onUpdatePressedNumber2 = async () => {
+    const { truth } = await viewTruth(walletAddress, id);
+    setViewTruth(truth);
+  };
+
+  const onUpdatePressedNumber3 = async () => {
+    const { status } = await mintVoters(walletAddress, voter);
+    setStatus(status);
+  };
+
+  const onUpdatePressedNumber4 = async () => {
+    const { status } = await viewTitle(walletAddress, id);
+    setStatus(status);
+  };
+
+  const onUpdatePressedNumber5 = async () => {
+    const { status } = await voteTruth(walletAddress, id, torf);
+    setStatus(status);
+  };
+
+  const onUpdatePressedNumber6 = async () => {
+    const { status } = await returnvotes(walletAddress, voter);
+    setStatus(status);
+  };
+
+  const onUpdatePressedNumber7 = async () => {
+    const { status } = await tallyVotesVerifyTruth(walletAddress, id, torf);
+    setStatus(status);
+  };
+
+  //the UI of our component
+  return (
+    <div id="container">
+      <img id="logo" src={alchemylogo}></img>
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <h2 style={{ paddingTop: "50px" }}>The Truth Machine: Anyone can submit a research proposal, article, etc. The selected group of voters can decide if the submission is "True", or research performed in good faith. This url/citation link can be verified with a function for further verification in other paper citations and etc. A truth consensus/verification machine.</h2>
+      <p>{truth}</p>
+
+      <h2 style={{ paddingTop: "18px" }}>Submit Truth Proposal!:</h2>
+
+      <div>
+        <label> TITLE
+        <input
+          type="text"
+          placeholder="Submit A New Truth Title"
+          onChange={(e) => setNewTruthTitle(e.target.value)}
+          value={newTruthTitle}
+        />
+        </label>
+        <br />
+        <label> URL
+        <input
+          type="text"
+          placeholder="Submit A New Truth URL"
+          onChange={(e) => setNewTruthUrl(e.target.value)}
+          value={newTruthURL}
+        />
+        </label>
+        <br />
+        <p id="status">{status}</p>
+
+        <button id="publish" onClick={onUpdatePressed}>
+          Update
+        </button>
+      </div>
+
+      <h2 style={{ paddingTop: "18px" }}>Vote On Truth Proposal (Must Be Approved Voter Transaction Will Revert)</h2>
+      <div>
+        <label> Truth Proposal ID No.
+        <input
+          type="text"
+          placeholder="Submit A New Truth Id No."
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+        />
+        </label>
+        <br />
+        <label> True Or False
+        <input
+          type="text"
+          placeholder="Enter True or False"
+          onChange={(e) => setTOrF(e.target.value)}
+          value={torf}
+        />
+        </label>
+        <br />
+        <p id="status">{status}</p>
+
+        <button id="publish1" onClick={onUpdatePressedNumber5}>
+          VOTE!
+        </button>
+      </div>
+
+      <p>{truth}</p>
+
+      <div>
+        <label> View Truth Boolean:
+        <input
+          type="text"
+          placeholder="Submit A Truth ID No."
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+        />
+        </label>
+        <br />
+        <button id="publish2" onClick={onUpdatePressedNumber2}>
+          Return Truth Value
+        </button>
+      </div>
+
+      <div>
+        <label> View Title By ID No.
+        <input
+          type="text"
+          placeholder="Submit A Truth ID No."
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+        />
+        </label>
+        <br />
+        <button id="publish3" onClick={onUpdatePressedNumber4}>
+          Return Truth Title
+        </button>
+      </div>
+      <div>
+        <label> View Truth Votes:
+        <input
+          type="text"
+          placeholder="Submit A Truth ID No."
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+        />
+        </label>
+        <br />
+        <button id="publish5" onClick={onUpdatePressedNumber6}>
+          Return Truth Vote Count
+        </button>
+      </div>
+
+      <div>
+        <label> Execute Vote Tally Determine If True:
+        <input
+          type="text"
+          placeholder="Submit A Truth ID No."
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+        />
+        </label>
+        <br />
+        <button id="publish6" onClick={onUpdatePressedNumber7}>
+          Tally Votes Flip Boolean!
+        </button>
+      </div>
+
+      <h2 style={{ paddingTop: "50px" }}>Mint A Voter (Admin Only)</h2>
+      <div>
+        <label> Mint Voter
+        <input
+          type="text"
+          placeholder="Address"
+          onChange={(e) => setNewVoter(e.target.value)}
+          value={voter}
+        />
+        </label>
+        <br />
+        <button id="publish4" onClick={onUpdatePressedNumber3}>
+          Mint
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HelloWorld;
---
+++ hello-world-part-four-tutorial-here/starter-files/src/util/interact.js
@@ -0,0 +1,376 @@
+require("dotenv").config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x2aa06a5B99b4969AE575cf64E71D32418D045202";
+
+export const helloWorldContract = new web3.eth.Contract(
+  contractABI,
+  contractAddress
+);
+
+
+export const submitTruth = async (address, message, url) => {
+
+    //input error handling
+    if (!window.ethereum || address === null) {
+      return {
+        status:
+          "💡 Connect your Metamask wallet to update the message on the blockchain.",
+      };
+    }
+  
+    if (message.trim() === "") {
+      return {
+        status: "❌ Your message cannot be an empty string.",
+      };
+    }
+  
+    //set up transaction parameters
+    const transactionParameters = {
+      to: contractAddress, // Required except during contract publications.
+      from: address, // must match user's active address.
+      data: helloWorldContract.methods.submitTruth(url, message).encodeABI(),
+    };
+  
+    //sign the transaction
+    try {
+      const txHash = await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+      });
+      return {
+        status:"SUCCESS",
+      };
+    } catch (error) {
+      return {
+        status: "😥 " + error.message,
+      };
+    }
+};
+
+export const mintVoters = async (address, toaddress) => {
+
+  //input error handling
+  if (!window.ethereum || address === null) {
+    return {
+      status:
+        "💡 Connect your Metamask wallet to update the message on the blockchain.",
+    };
+  }
+
+  if (toaddress.trim() === "") {
+    return {
+      status: "❌ Your message cannot be an empty string.",
+    };
+  }
+
+  //set up transaction parameters
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.mintVoters(toaddress).encodeABI(),
+  };
+
+  //sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status:"SUCCESS",
+      txHash,
+    };
+  } catch (error) {
+    return {
+      status: "😥 " + error.message,
+    };
+  }
+};
+
+export const voteTruth = async (address, truthid, bool) => {
+
+  //input error handling
+  if (!window.ethereum || address === null) {
+    return {
+      status:
+        "💡 Connect your Metamask wallet to update the message on the blockchain.",
+    };
+  }
+
+  if (truthid.trim() === "") {
+    return {
+      status: "❌ Your id cannot be an empty string.",
+    };
+  }
+
+  //set up transaction parameters
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.voteTruth(truthid, bool).encodeABI(),
+  };
+
+  //sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status: "SUCCESS",
+      txHash,
+    };
+  } catch (error) {
+    return {
+      status: "😥 " + error.message,
+    };
+  }
+
+};
+export const tallyVotesVerifyTruth = async (address, truthid) => {
+
+  //input error handling
+  if (!window.ethereum || address === null) {
+    return {
+      status:
+        "💡 Connect your Metamask wallet to update the message on the blockchain.",
+    };
+  }
+
+  if (truthid.trim() === "") {
+    return {
+      status: "❌ Your id cannot be an empty string.",
+    };
+  }
+
+  //set up transaction parameters
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.tallyVotesVerifyTruth(truthid).encodeABI(),
+  };
+
+  //sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status: "SUCCESS",
+      txHash,
+    };
+  } catch (error) {
+    return {
+      status: "😥 " + error.message,
+    };
+  }
+
+};
+
+export const returnvotes = async (address, truthid) => {
+
+  //input error handling
+  if (!window.ethereum || address === null) {
+    return {
+      status:
+        "💡 Connect your Metamask wallet to update the message on the blockchain.",
+    };
+  }
+
+  if (truthid.trim() === "") {
+    return {
+      status: "❌ Your message cannot be an empty string.",
+    };
+  }
+
+  //set up transaction parameters
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.returnvotes(truthid).encodeABI(),
+  };
+
+  //sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status:"SUCCESS",
+      txHash,
+    };
+  } catch (error) {
+    return {
+      status: "😥 " + error.message,
+    };
+  }
+};
+
+//const truth = await helloWorldContract.methods.viewTruth(truthid).call();
+//    return truth;
+
+export const viewTruth = async (address, truthid) => {
+
+    //input error handling
+    if (!window.ethereum || address === null) {
+      return {
+        status:
+          "💡 Connect your Metamask wallet to update the message on the blockchain.",
+      };
+    }
+  
+    if (truthid.trim() === "") {
+      return {
+        status: "❌ Your id cannot be an empty string.",
+      };
+    }
+  
+    //set up transaction parameters
+    const transactionParameters = {
+      to: contractAddress, // Required except during contract publications.
+      from: address, // must match user's active address.
+      data: helloWorldContract.methods.viewTruth(truthid).encodeABI(),
+    };
+  
+    //sign the transaction
+    try {
+      const txHash = await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+      });
+      return {
+        status: "SUCCESS",
+        txHash,
+      };
+    } catch (error) {
+      return {
+        status: "😥 " + error.message,
+      };
+    }
+
+};
+
+export const viewTitle = async (address, truthid) => {
+
+  //input error handling
+  if (!window.ethereum || address === null) {
+    return {
+      status:
+        "💡 Connect your Metamask wallet to update the message on the blockchain.",
+    };
+  }
+
+  if (truthid.trim() === "") {
+    return {
+      status: "❌ Your id cannot be an empty string.",
+    };
+  }
+
+  //set up transaction parameters
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.viewTitle(truthid).encodeABI(),
+  };
+
+  //sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status: "SUCCESS",
+      txHash,
+    };
+  } catch (error) {
+    return {
+      status: "😥 " + error.message,
+    };
+  }
+
+};
+
+export const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const obj = {
+          status: "👆🏽 Write a message in the text-field above.",
+          address: addressArray[0],
+        };
+        return obj;
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
+  
+  export const getCurrentWalletConnected = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (addressArray.length > 0) {
+          return {
+            address: addressArray[0],
+            status: "👆🏽 Write a message in the text-field above.",
+          };
+        } else {
+          return {
+            address: "",
+            status: "🦊 Connect to Metamask using the top right button.",
+          };
+        }
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
